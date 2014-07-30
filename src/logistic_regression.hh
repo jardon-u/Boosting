bb9e967b852bcdc0e4c909e5dcd3850191baba88
Ugo Jardonnet <u.jardonnet@criteo.com>
2014-07-30 22:25:30 +0200

    stub of logistic reg

--- src/logistic_regression.hh
+++ src/logistic_regression.hh
@@ -9,7 +9,20 @@ namespace classification
   {
     typedef int    label_t;  ///< label type.
     typedef double weight_t; ///< weight type.
-    typedef T      point_t;  ///< point type.    
+    typedef T      point_t;  ///< point type.
+
+    double convergence_rate;
+    double learning_rate;
+    int max_iters;
+
+    logistic_regression(double convergence_rate_ = 0.0001,
+                        double learning_rate_    = 0,00005,
+                        int    max_iter_         = 2000)
+      : convergence_rate(convergence_rate),
+        learning_rate(learning_rate),
+        max_iter(max_iter_)
+    {
+    }
 
     /// Fit classifier to data
     /// \p x_ data
@@ -19,6 +32,42 @@ namespace classification
               const std::vector<label_t>& y,
               std::vector<weight_t>&      w )
     {
+      int iter = 0;
+
+      // init
+      std::vector<double> weight_old(x[0].size(), 0);
+      std::vector<double> weight_new(x[0].size(), 0);
+
+      while (iter <= max_iters)
+      {
+        for (std::size_t k = 0; k < weight_new.size(); ++k)
+        {
+          double gradient = 0;
+          for (std::size_t i = 0; i < x.size(); ++i)
+          {
+            double z_i = 0;
+            for (std::size_t j = 0; j < weight_old.size() && iter != 0; ++j)
+            {
+              z_i += weight_old[j] * x[i][j];
+            }
+            gradient = y[i] * x[i][k] * sigmoid(-y[i] * z_i);
+          }
+          weight_new[k] = weight_old(k) + learning_rate * gradient;
+        }
+
+        double dist = norm(weight_new, weight_old);
+        if (dist < convergence_rate)
+        {
+          cout << "the best weight: " << weight_new << endl;
+          break;
+        }
+        else
+        {
+          weight_old.swap(weight_new);
+        }
+
+        iter++;
+      }
     }
 
     /// Apply classifier. return a label
